5e30730f02bd5789720ecdc446d825a60f97af31
Vircsr <39640022+Vircsr@users.noreply.github.com>
2018-06-01 10:35:37 -0500

    Add files via upload

--- ball_in_box/ballinbox.py
+++ ball_in_box/ballinbox.py
@@ -20,7 +20,7 @@ def ball_in_box(m, blockers):
         x = 0
         y = 0
         i = 0
-        while i < 10000:
+        while i < 50000:
             rf = 2.0
             xf = random.random() * 2 - 1
             yf = random.random() * 2 - 1
@@ -51,7 +51,7 @@ def ball_in_box(m, blockers):
             else:
                 i = i + 1
         circles.append((x, y, r))
-        print(r)
+
         if not validate(circles, blockers):
             print('There are some thing wrong')
         circle_index += 1
